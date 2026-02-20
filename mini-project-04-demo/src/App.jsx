87dc1701ef48b7ff5085d93f46d94c836a9e588b
ZupperZane <72839201+ZupperZane@users.noreply.github.com>
2026-02-20 16:18:26 -0500

    Update App.jsx
    Added Dropdown, Functionality and search

--- mini-project-04-demo/src/App.jsx
+++ mini-project-04-demo/src/App.jsx
@@ -3,9 +3,19 @@ import "./App.css";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [error, setError] = useState(null);
-
+  //Filter 1 is the Category
+  const [filter1, setFilter1] = useState("title");
+  //Search
+  const searchQuery = "" ;
+  //Filtered Data
+const filteredData = data.filter(movie =>
+  String(movie[filter1] ?? "")
+    .toLowerCase()
+    .includes(searchQuery.toLowerCase())
+);
+  //Fetch Function
   const fetchData = useCallback(async () => {
     try {
       const response = await fetch(
@@ -30,17 +40,33 @@ const App = () => {
   }, [fetchData]);
 
   return (
-    <>
-      <h1>Mini Project 04</h1>
+<>
+    <select value ={filter1}  onChange={(e) => setFilter1(e.target.value)}>
+      <option value="title">Title</option>
+      <option value="director">Director</option>
+      <option value="releasing_year">Year Released</option>
+      <option value="language">Language</option>
+      <option value="runtime">Runtime</option>
+      <option value="age_group">Age Group</option>
+      <option value="short_description">Description</option>
+      <option value="genre">Genre</option>
+      <option value="budget">Budget</option>
+      <option value="imdb_rating">Rating (IMDB)</option>
+    </select>
+
+    <search>
+      
+    </search>
+
       {isLoading && <h1 className="text-7xl">Loading data ... please wait</h1>}
-      {error && <h1 className="text-3xl text-red-600">Error: {error.message}</h1>}
       {!isLoading && data && (
         <ul className="m-10 p-3">
-          {data.map((d, index) => (
-            <li key={index} className="text-3xl">
-              {d.title}
-            </li>
-          ))}
+         {data.map((d, index) => (
+  <li key={index} className="text-3xl">
+    {d[filter1]}
+  </li>
+))}
+
         </ul>
       )}
     </>
